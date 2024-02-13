f3e36e0b6d132140488e30535d0b53ced38d81a2
feerraan <fazpiazuadrover@gmail.com>
2024-02-13 12:53:28 +0100

    pr1

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -36,7 +36,7 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalinput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         verticalinput = Input.GetAxis("Vertical");
 
